5901db0b7d053f702d3af5e27000e6078b71e818
Điền Trần <dientran0410@gmail.com>
2026-01-22 21:34:33 +0700

    create service, serviceImpl, repository package

---
+++ Project/BE/src/main/java/com/example/project/ServiceImpl/CategoryServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.project.ServiceImpl;
+
+import com.example.project.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+}
---
+++ Project/BE/src/main/java/com/example/project/ServiceImpl/ProductServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.project.ServiceImpl;
+
+import com.example.project.service.ProductService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+}

e235c678496d6dfc949738daeceb649bdf64c9c8
Duy-Phan-prg <phanduy.prg@gmail.com>
2026-02-06 07:21:54 +0700

    d

--- Project/BE/src/main/java/com/example/project/config/WebConfig.java
+++ Project/BE/src/main/java/com/example/project/config/WebConfig.java
@@ -1,11 +1,21 @@
 package com.example.project.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // deploy xong sẽ thay domain vercel
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
